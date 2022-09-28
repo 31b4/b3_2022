7a48a31d3d8465053dbe6477af4c749802530811
31b4 <75566095+31b4@users.noreply.github.com>
2022-09-28 14:02:38 +0200

    Add files via upload

--- sajat_web/DEscript.js
+++ sajat_web/DEscript.js
@@ -3,16 +3,18 @@ $(document).ready(function (params) {
     setInterval(()=>{Igazitas();}, 100)
     
     function Igazitas(params) {            
-        if ($(window).width() > 870) {
+        if ($(window).width() > 1000) {
             $("#tagok div").css("padding-top", "20px")
+            $("#tagok div").removeClass("col-12");
         }
-        else if ($(window).width() > 650) {
+        else if ($(window).width() > 690) {
             $("#tagok div").css("padding-top", "35px")
             $("#tagok div").removeClass("col-12");
         }
         else{
             $("#tagok div").addClass("col-12");
             $("#tagok div").css("padding-top", "50px")
+            $("#tagok div#elsoKep").css("padding-top","20px")
         }
     }            
 })
